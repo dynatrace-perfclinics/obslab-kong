d333f71ce8f7aba322e4671953b30e1d4fe1f2fa
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-08-06 02:00:22 +0000

    add dynakube

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -21,12 +21,23 @@ OTEL_DEMO_VERSION=0.32.3
 # Replace placeholders in helm-values.yaml with realtime values
 sed -i "s,CODESPACE_NAME_PLACEHOLDER,$CODESPACE_NAME," .devcontainer/otel-demo/helm-values.yaml
 sed -i "s,GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN_PLACEHOLDER,$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN," .devcontainer/otel-demo/helm-values.yaml
+# Replace placeholders in dynakube.yaml with realtime values
+sed -i "s,CODESPACE_NAME_PLACEHOLDER,$CODESPACE_NAME," .devcontainer/dynatrace/dynakube.yaml
+sed -i "s,DT_ENDPOINT_PLACEHOLDER,$DT_ENDPOINT_OBSLAB_KONG," .devcontainer/dynatrace/dynakube.yaml
 
 # Create Cluster
 kind create cluster --config .devcontainer/kind-cluster.yaml --wait $CREATE_CLUSTER_WAIT
 
 # Store DT Details in a secret (used by OTEL collector for data ingest)
 kubectl create secret generic dt-details --from-literal=DT_ENDPOINT_OBSLAB_KONG=$DT_ENDPOINT_OBSLAB_KONG --from-literal=DT_API_TOKEN_OBSLAB_KONG=$DT_API_TOKEN_OBSLAB_KONG
+
+# Install Dynatrace Operator + ActiveGate
+#   Note: Data Ingest token only has metrics.ingest permission
+#     So re-use DT_API_TOKEN_OBSLAB_KONG instead of asking users for yet another token
+kubectl create namespace dynatrace
+kubectl create secret generic cluster-$CODESPACE_NAME --from-literal=apiToken=$DT_OPERATOR_TOKEN_OBSLAB_KONG --from-literal=dataIngestToken=$DT_API_TOKEN_OBSLAB_KONG
+helm install dynatrace-operator oci://public.ecr.aws/dynatrace/dynatrace-operator --namespace dynatrace --atomic
+kubectl apply -f .devcontainer/dynatrace/dynakube.yaml
 
 # Add OTEL demo using Helm
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
@@ -41,6 +52,8 @@ kubectl create namespace kong
 kubectl create secret generic kong-enterprise-license --from-literal=license="'{}'" -n kong
 openssl req -new -x509 -nodes -newkey ec:<(openssl ecparam -name secp384r1) -keyout ./tls.key -out ./tls.crt -days 1095 -subj "/CN=kong_clustering"
 kubectl create secret tls kong-cluster-cert --cert=./tls.crt --key=./tls.key -n kong
+# Remove sensitive files - no longer needed
+rm tls.crt tls.key
 helm install kong-cp kong/kong -n kong --values /workspaces/obslab-kong/.devcontainer/kong/values-cp.yaml
 
 # Install kong data plane
